0e48a68ce9723e0942d15eeeaf8eea274a5ebf5e
LimboWRLD <namidzic34@gmail.com>
2025-11-20 05:17:19 +0100

    Add employee crud, add keycloak client for backend

--- frontend/andrea360-web/src/app/admin/admin/admin-routing.module.ts
+++ frontend/andrea360-web/src/app/admin/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LocationComponent } from '../location/location.component';
+import { EmployeeComponent } from '../employee/employee.component';
 
 const routes: Routes = [
-  {path:"locations", component:LocationComponent}
+  {path:"locations", component:LocationComponent},
+  {path:"employees", component:EmployeeComponent}
 ];
 
 @NgModule({
